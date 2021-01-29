aace5e309d8ed99e2fe3e4bb72455ec1015bf425
Amakine <50359104+Amakine@users.noreply.github.com>
2021-01-29 22:59:56 +0200

    json-server

--- osa2/notes/src/index.js
+++ osa2/notes/src/index.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import axios from 'axios'
 
-const notes = [
-  {
-    id: 0,
-    content: 'juu',
-    important: true
-  },
-  {
-    id: 1,
-    content: 'jaa',
-    important: false
-  }
-]
+const promise = axios.get(
+  'http://localhost:3001/notes'
+)
 
+promise.then(response => {
+  console.log(JSON.stringify(response, null, '\t'))
+  console.log(response.data)
+})
+
+const promise2 = axios.get(
+  'http://localhost:3001/foo'
+)
+console.log(promise2);
+
+
+
+/*
 ReactDOM.render(
-  <App notes={notes} />,
+  <App />,
   document.getElementById('root')
 )
+*/
