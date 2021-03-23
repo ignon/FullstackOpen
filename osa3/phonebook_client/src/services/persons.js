d732e94e447379894eaa25ff2c7292242d9df9b8
Arttu Mäkinen <ignomagames@gmail.com>
2021-03-23 16:00:46 +0200

    backup

--- osa3/phonebook_client/src/services/persons.js
+++ osa3/phonebook_client/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'//'https://arden-servu.herokuapp.com/api/persons'
+const baseUrl = 'https://arden-servu.herokuapp.com/api/persons'
 
 const getAll = () => {
     return axios.get(baseUrl)
